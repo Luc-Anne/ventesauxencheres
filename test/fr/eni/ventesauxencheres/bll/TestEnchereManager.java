6668721b415bb021f5863f241b53fa8d27f72dd8
lanne2022 <lanne2022@105SE32-2TBVVD3.ad.campus-eni.fr>
2022-08-17 17:28:47 +0200

    Ajout des fichiers de tests du Modèle Ajout des fichiers de tests des implementations jdbc de la DAL et des managers de la BLL. Incorporation du test de connexion dans la trame de test défini.

--- test/fr/eni/ventesauxencheres/bll/TestEnchereManager.java
+++ test/fr/eni/ventesauxencheres/bll/TestEnchereManager.java
@@ -1,8 +1,7 @@
-package fr.eni.ventesauxencheres.dal;
+package fr.eni.ventesauxencheres.bll;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,11 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TestConnectionProvider
- */
-@WebServlet("/test/TestConnectionProvider")
-public class TestConnectionProvider extends HttpServlet {
+@WebServlet("/test/TestEnchereManager")
+public class TestEnchereManager extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -23,37 +19,28 @@ public class TestConnectionProvider extends HttpServlet {
 		
 		// Test Method 1
 		// Test 1 ###################################################
-		pw.append("\nTest 1 : Connexion à la base de données\n");
+		pw.append("\nTest 1 : \n");
 		message = "";
-		
-		try {
-			ConnectionProvider.getConnection_VAE();
-			message += "réussi";
-		} catch (SQLException e) {
-			e.printStackTrace();
-			message += "échec :" + e.getMessage();
-		}
-       
+
 		pw.append(message);
 		// Test 2 ###################################################
 		pw.append("\nTest 2 : \n");
 		message = "";
-	
+
 		pw.append(message);
 		// Test Method 2
 		// Test 3 ###################################################
 		pw.append("\nTest 3 : \n");
 		message = "";
-	
+
 		pw.append(message);
 		// Test 4 ###################################################
 		pw.append("\nTest 4 : \n");
 		message = "";
-	
+
 		pw.append(message);
 		// Display test results #####################################
 		pw.flush();
 	}
+
 }
-
-
