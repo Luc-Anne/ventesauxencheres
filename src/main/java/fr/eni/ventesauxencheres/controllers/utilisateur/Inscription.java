3ec29a6a0446b8974e65f397c1825d379710a775
lanne2022 <lanne2022@105SE32-2TBVVD3.ad.campus-eni.fr>
2022-08-18 14:11:17 +0200

    - Création du fragment taglib.jspf pour avoir les taglib pour les servlets en évitant les erreurs de redéfinitions levées. - Création de la class Errors pour remonter les erreurs dans les jsp à partir des controllers et de la validation métier du modèle. - Mise en place de la validation des utilisateurs pour ne pas corrompre les données en base de données et remonter les erreurs potentielles vers la jsp. - Gestion des erreurs pour la fonctionnalité Inscription. - Nettoyage du code inscription.jsp en se basant sur la documentation bootstrap.

--- src/main/java/fr/eni/ventesauxencheres/controllers/utilisateur/Inscription.java
+++ src/main/java/fr/eni/ventesauxencheres/controllers/utilisateur/Inscription.java
@@ -1,6 +1,7 @@
 package fr.eni.ventesauxencheres.controllers.utilisateur;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import fr.eni.ventesauxencheres.bll.BLLException;
 import fr.eni.ventesauxencheres.bll.UtilisateurManager;
 import fr.eni.ventesauxencheres.bo.Utilisateur;
+import fr.eni.ventesauxencheres.controllers.Errors;
 
 /**
  * Servlet implementation class Inscription
@@ -37,20 +39,29 @@ public class Inscription extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		Errors erreurs = new Errors();
+		request.setAttribute("erreurs", erreurs);
+		
 		HttpSession session = request.getSession();
 
 		// (pseudo, nom,prenom,
 		// email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur)
-		String pseudo = request.getParameter("pseudo");
-		String nom = request.getParameter("nom");
-		String prenom = request.getParameter("prenom");
-		String email = request.getParameter("email");
-		String telephone = request.getParameter("telephone");
-		String rue = request.getParameter("rue");
-		String code_postal = request.getParameter("code_postal");
-		String ville = request.getParameter("ville");
-		String mot_de_passe = request.getParameter("password");
-		int credit = Integer.parseInt(request.getParameter("credit"));
+		String pseudo = request.getParameter("pseudo") != null ? request.getParameter("pseudo") : null;
+		String nom = request.getParameter("nom") != null ? request.getParameter("nom") : null;
+		String prenom = request.getParameter("prenom") != null ? request.getParameter("prenom") : null;
+		String email = request.getParameter("email") != null ? request.getParameter("email") : null;
+		String telephone = request.getParameter("telephone") != null ? request.getParameter("telephone") : null;
+		String rue = request.getParameter("rue") != null ? request.getParameter("rue") : null;
+		String code_postal = request.getParameter("code_postal") != null ? request.getParameter("code_postal") : null;
+		String ville = request.getParameter("ville") != null ? request.getParameter("ville") : null;
+		String mot_de_passe = request.getParameter("password") != null ? request.getParameter("password") : null;
+		int credit = 0;
+		try {
+			credit = request.getParameter("credit") != null ? Integer.parseInt(request.getParameter("credit")) : 0;
+		} catch (NumberFormatException e) {
+			credit = 0;
+			erreurs.add("utilisateur.credit_not_int");
+		}
 		boolean administrateur = false;
 
 		if (request.getParameter("status") == null) {
@@ -62,27 +73,32 @@ public class Inscription extends HttpServlet {
 
 		Utilisateur u = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe,
 				credit, administrateur);
+		boolean entergistre = false;
 		try {
-			boolean entergistre = UtilisateurManager.getInstance().inscription(u);
-			if (entergistre) {
-				session.setAttribute("messageInscription", "Inscription réussie ");
-				// faire une redirection
-				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
-
-				if (rd != null) {
-					rd.forward(request, response);
-				}
-			} else {
-				session.setAttribute("messageInscription", "Inscription Echoué");
-//				request.setAttribute("messageConnexion", "Email ou mot de passe incorrect.");
-				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/utilisateur/inscription.jsp");
-				if (rd != null) {
-					rd.forward(request, response);
-				}
-			}
+			entergistre = UtilisateurManager.getInstance().inscription(u);
 		} catch (BLLException e) {
-
+			erreurs.addAll(UtilisateurManager.getInstance().invalidCause(u));
 			e.printStackTrace();
+		}
+		for (String erreur : erreurs.getErreurs()) {
+			System.out.println(erreur);
+		}
+		
+		if (entergistre) {
+			session.setAttribute("messageInscription", "Inscription réussie ");
+			// faire une redirection
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
+
+			if (rd != null) {
+				rd.forward(request, response);
+			}
+		} else {
+			session.setAttribute("messageInscription", "Inscription Echoué");
+//			request.setAttribute("messageConnexion", "Email ou mot de passe incorrect.");
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/utilisateur/inscription.jsp");
+			if (rd != null) {
+				rd.forward(request, response);
+			}
 		}
 
 	}
