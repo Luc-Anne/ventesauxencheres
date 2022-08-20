c41b35e4225956c343397c7a44017137514a205b
lanne2022 <lanne2022@105SE32-2TBVVD3.ad.campus-eni.fr>
2022-08-20 08:49:02 +0200

    Gestion de projet - Ajout Nota Bene fichier TODO - Ajout d'un template pour les Servlets

---
+++ docs/TemplateServlet.java
@@ -0,0 +1,31 @@
+package fr.eni.ventesauxencheres.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Template
+ */
+@WebServlet("/$$$$")
+public class $$$$ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* 1. Récupérer les paramètres
+		   2. Vérifier que les paramètres sont conformes au cas spécifiques de la Servlet (les cas généraux seront gérés grâce aux validation dans les BLL)
+		   3. Suivant les paramètres, les attributs disponibles dans les servlet et les données provenant d'appels à la BLL,
+		      Renvoyer une vue spécifique à l'utilisateur, ce que soit en passant par une autre servlet ou bien par un appel une jsp.
+		      Au cours de ce cheminement vers la vue, sauvegarder éventuellement des données dans des attributs ou bien grâve à des appels à la BLL
+		*/
+		
+	}
+
+}
