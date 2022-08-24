e5f36865d63b6cc74f5d70fe2b048ee1eb0395a1
btruong2022 <btruong2022@105SE32-8T0TVD3.ad.campus-eni.fr>
2022-08-24 12:13:13 +0200

    Rappatriement du code pour gérer l'affichage des articles par filtres dans la methode post de la servlet home

--- src/main/java/fr/eni/ventesauxencheres/controllers/navigation/Home.java
+++ src/main/java/fr/eni/ventesauxencheres/controllers/navigation/Home.java
@@ -37,6 +37,38 @@ public class Home extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//récupérer la valeur du bouton radio coché et la renvoyer dans la requête
+		String typeEncheres = request.getParameter("type-encheres");
+		request.setAttribute("typeEncheres", typeEncheres);		
+		//récupérer les paramètres des checkbox et les renvoyer
+		String encheresStatut = request.getParameter("encheres");
+		request.setAttribute("encheresStatut", encheresStatut);
+		String ventesStatut = request.getParameter("ventes");
+		request.setAttribute("ventesStatut", ventesStatut);
+		//Appeler la liste des achats avec encheres ouvertes
+		try {
+			List<Article> encheresOuvertesListe=ArticleManager.getInstance().showOpenedBids();
+			List<Article> MesEncheresListe=ArticleManager.getInstance().showMyBids();
+			List<Article> MesEncheresGagneesListe=ArticleManager.getInstance().showMyWonBids();
+			List<Article> MesVentesEncoursListe=ArticleManager.getInstance().showMyCurrentSales();
+			List<Article> MesVentesNonDebuteesListe=ArticleManager.getInstance().showMyUnstartedSales();
+			List<Article> MesVentesTermineesListe=ArticleManager.getInstance().showMyClosedSales();
+			//Envoyer la liste récupérée
+			request.setAttribute("encheresOuvertesListe", encheresOuvertesListe);
+			request.setAttribute("MesEncheresListe", MesEncheresListe);
+			request.setAttribute("MesEncheresGagneesListe", MesEncheresGagneesListe);
+			request.setAttribute("MesVentesEncoursListe", MesVentesEncoursListe);
+			request.setAttribute("MesVentesNonDebuteesListe", MesVentesNonDebuteesListe);
+			request.setAttribute("MesVentesTermineesListe", MesVentesTermineesListe);
+		} catch (BLLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
+		if (rd != null) {
+			rd.forward(request, response);
+		}		
 	}
 
 }
