45cca9f17a0d29b22cadffb2d941051125c34057
lanne2022 <lanne2022@105SE32-1R0TVD3.ad.campus-eni.fr>
2022-09-08 15:35:49 +0200

    use only mariadb

--- src/main/java/fr/eni/ventesauxencheres/dal/ConnectionProvider.java
+++ src/main/java/fr/eni/ventesauxencheres/dal/ConnectionProvider.java
@@ -10,17 +10,9 @@ import javax.sql.DataSource;
 
 public class ConnectionProvider {
 
-	private static DataSource bdd_SQLSERVER_ENIVEA;
 	private static DataSource bdd_MARIADB_ENIVEA;
 
 	static {
-		try {
-			Context context = new InitialContext();
-			bdd_SQLSERVER_ENIVEA = (DataSource)context.lookup("java:comp/env/jdbc/sqlServer/pool_cnx_ENIVAE");
-			System.out.println("Contexte de base de données : SQL SERVER");
-		} catch (NamingException e) {
-			//e.printStackTrace();
-		}
 		try {
 			Context context = new InitialContext();
 			bdd_MARIADB_ENIVEA = (DataSource)context.lookup("java:comp/env/jdbc/mariaDB/pool_cnx_ENIVAE");
@@ -31,13 +23,7 @@ public class ConnectionProvider {
 	}
 
 	public static Connection getConnection_VAE() throws SQLException {
-		if (bdd_SQLSERVER_ENIVEA != null) {
-			return bdd_SQLSERVER_ENIVEA.getConnection();
-		}
-		if (bdd_MARIADB_ENIVEA != null) {
-			return bdd_MARIADB_ENIVEA.getConnection();
-		}
-		return null;
+		return bdd_MARIADB_ENIVEA.getConnection();
 	}
 
 }
